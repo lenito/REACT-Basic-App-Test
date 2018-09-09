e1a4a01dd534baec44aebc60811d7d386427fcd8
Lenito Ribeiro <lenito@gmail.com>
2018-09-09 19:35:44 +1200

    Initial commit

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+    <div>
+        <PageHeader name='Sobre' small='Nós'> </PageHeader>
+        <h2> Nossa história </h2>
+        <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, nisi maiores! Sit ratione odit, alias non provident, iure ducimus deleniti aspernatur, eius fuga atque in hic expedita possimus quos nobis! </p>
+        <h2> Missão </h2>
+        <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, nisi maiores! Sit ratione odit, alias non provident, iure ducimus deleniti aspernatur, eius fuga atque in hic expedita possimus quos nobis! </p>
+        <h2> Imprensa </h2>
+        <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, nisi maiores! Sit ratione odit, alias non provident, iure ducimus deleniti aspernatur, eius fuga atque in hic expedita possimus quos nobis! </p>
+    </div>
+)
